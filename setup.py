0e6c2baa30ecb2f738e82c391ea7becf19333c95
Mark Williams <mark.c.will@gmail.com>
2012-12-12 13:52:29 -0800

    Fixed setup to handle dbhash script

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
 
-setup(name='HASHpy',
-      version='1.0',
-      description='Routines for running HASH algorithms',
-      author='Mark Williams',
-      url='https//github.com/markcwill',
-      packages=['hashpy', 'hashpy.db'],
-      package_data={'hashpy':['libhashpy.so','src/*.inc','Makefile','data/*', 'src/*.f']},
-)
+kwargs = {name='HASHpy',
+          version='1.0',
+          description='Routines for running HASH algorithms',
+          author='Mark Williams',
+          url='https//github.com/markcwill',
+          packages=['hashpy', 'hashpy.db'],
+          package_data={'hashpy':['libhashpy.so','src/*.inc','Makefile','data/*', 'src/*.f']},
+}
 
+if 'ANTELOPE' in os.environ:
+    ant_bin = os.join(os.environ['ANTELOPE'], 'bin')
+    kwargs['data_files'] = [(ant_bin, ['db/dbhash'])]
 
+setup(**kwargs)
