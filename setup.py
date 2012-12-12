9350b0c54a5c89490f31b47516dd3ecbfd193631
Mark Williams <mark.c.will@gmail.com>
2012-12-12 13:57:36 -0800

    Typo in setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from distutils.core import setup
 import os
 
-kwargs = {name : 'HASHpy',
-          version : '1.0',
-          description : 'Routines for running HASH algorithms',
-          author : 'Mark Williams',
-          url : 'https//github.com/markcwill',
-          packages : ['hashpy', 'hashpy.db'],
-          package_data : {'hashpy':['libhashpy.so','src/*.inc','Makefile','data/*', 'src/*.f']},
+kwargs = {'name' : 'HASHpy',
+          'version' : '1.0',
+          'description' : 'Routines for running HASH algorithms',
+          'author' : 'Mark Williams',
+          'url' : 'https//github.com/markcwill',
+          'packages' : ['hashpy', 'hashpy.db'],
+          'package_data' : {'hashpy':['libhashpy.so','src/*.inc','Makefile','data/*', 'src/*.f']},
 }
 
 if 'ANTELOPE' in os.environ:
