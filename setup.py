f12a835a3dd85c3c0e9b55d9fe329d567c592079
Mark Williams <mark.c.will@gmail.com>
2013-04-15 18:07:08 -0700

    Removed ancient Makefile technology, compile fortran lib in setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from numpy.distutils.core import setup, Extension
 import os
 
-kwargs = {'name'         : 'HASHpy',
-          'version'      : '1.0',
+# Build extension based on the fucntion numpy.f2py.f2py2e.run_compile
+srcf = ['fmech_subs.f', 'uncert_subs.f', 'util_subs.f',
+        'pol_subs.f', 'vel_subs.f', 'station_subs.f', 'vel_subs2.f' ]
+
+src_list = ['hashpy/src/' + src for src in srcf]
+
+ext = Extension('hashpy.libhashpy', sources=src_list)
+
+# Regular setup stuff
+s_args = {'name'         : 'HASHpy',
+          'version'      : '2.0',
           'description'  : 'Routines for running HASH algorithms',
           'author'       : 'Mark Williams',
           'url'          : 'https//github.com/markcwill',
           'packages'     : ['hashpy', 'hashpy.db'],
-          'package_data' : {'hashpy':['libhashpy.so','src/*.inc','Makefile','data/*', 'src/*.f']},
+          'package_data' : {'hashpy':['src/*.inc','src/Makefile','data/*', 'src/*.f']},
+          'ext_modules'  : [ext],
 }
 
+# copy pf and bins from hashpy.db to antelope if available
 if 'ANTELOPE' in os.environ:
     ant_bin = os.path.join(os.environ['ANTELOPE'], 'bin')
     ant_pf  = os.path.join(os.environ['ANTELOPE'], 'data', 'pf')
-    kwargs['data_files'] = [(ant_bin, ['hashpy/db/dbhash']),
+    s_args['data_files'] = [(ant_bin, ['hashpy/db/dbhash']),
                             (ant_pf,  ['hashpy/db/dbhash.pf'])]
-
-setup(**kwargs)
+# Go
+setup(**s_args)
