99fa582b541ecb1a2131ae35228d680ae9771280
Mark Williams <mark.c.will@gmail.com>
2013-06-24 10:36:13 -0700

    Doc and added include folder variable prefix for antelope

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
+#
+# setup.py file for compiling HASH and installing hashpy
+#
 from numpy.distutils.core import setup, Extension
 import sys, os
 
 #--- libhashpy SETUP ---------------------------------------------------------
+#
 # Build extension based on the fucntion numpy.f2py.f2py2e.run_compile
 srcf = ['fmech_subs.f', 'uncert_subs.f', 'util_subs.f',
         'pol_subs.f', 'vel_subs.f', 'station_subs.f', 'vel_subs2.f' ]
@@ -10,17 +14,25 @@ src_list = ['hashpy/src/' + src for src in srcf]
 
 ext_args = { 'sources' : src_list }
 
-ANT_EXT_ARGS = {'include_dirs' : ['/opt/antelope/python2.7.2-64/include',
-                                  '/opt/antelope/python2.7.2-64/lib/python2.7/site-packages/numpy/core/include'],
-                'library_dirs' : ['/opt/antelope/python2.7.2-64/lib'], 
+#--- Special case includes for compiling/linking with Antelope python
+#
+# Use as a template for non-standard (non-distro) python installls...
+#
+python_folder = '/opt/antelope/python2.7.2-64'
+
+ANT_EXT_ARGS = {'include_dirs' : [ python_folder + '/include',
+                                   python_folder + '/lib/python2.7/site-packages/numpy/core/include'],
+                'library_dirs' : [ python_folder + '/lib'], 
                }
 
 if 'antelope' in sys.executable:
     ext_args.update(ANT_EXT_ARGS)
 
-ext = Extension('hashpy.libhashpy', **ext_args)
 #-----------------------------------------------------------------------------
-# Regular setup stuff
+
+ext = Extension('hashpy.libhashpy', **ext_args)
+
+### Regular setup stuff ######################################################
 
 s_args = {'name'         : 'HASHpy',
           'version'      : '2.0',
@@ -32,11 +44,17 @@ s_args = {'name'         : 'HASHpy',
           'ext_modules'  : [ext],
 }
 
+# hashpy.db ------------------------------------------------------------------
+# TO BE OBSELETED - break out to separate module/package so hashpy can
+# stand alone.
+#
 # copy pf and bins from hashpy.db to antelope if available
 if 'ANTELOPE' in os.environ:
     ant_bin = os.path.join(os.environ['ANTELOPE'], 'bin')
     ant_pf  = os.path.join(os.environ['ANTELOPE'], 'data', 'pf')
     s_args['data_files'] = [(ant_bin, ['hashpy/db/dbhash']),
                             (ant_pf,  ['hashpy/db/dbhash.pf'])]
+#-----------------------------------------------------------------------------
+
 # Go
 setup(**s_args)
